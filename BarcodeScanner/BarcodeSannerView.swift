2527c462df2c91d7b081f3c1c7752f55c56298fa
Bizet Rodriguez <brodriguez-velez@ucmerced.edu>
2020-11-24 22:37:11 -0800

    Creating main barcode scanner view.

---
+++ BarcodeScanner/BarcodeSannerView.swift
@@ -0,0 +1,38 @@
+//
+//  ContentView.swift
+//  BarcodeScanner
+//
+//  Created by Bizet Rodriguez on 11/24/20.
+//
+
+import SwiftUI
+
+struct BarcodeSannerView: View {
+    var body: some View {
+        NavigationView {
+            VStack {
+                Rectangle()
+                    .frame(maxWidth: .infinity, maxHeight: 300)
+                
+                Spacer()
+                    .frame(height: 60)
+                
+                Label("Scanned Barcode", systemImage: "barcode.viewfinder")
+                    .font(.title)
+                
+                Text("Not Yet Scanned")
+                    .bold()
+                    .font(.largeTitle)
+                    .foregroundColor(.green)
+                    .padding()
+            }
+            .navigationTitle("Barcode Scanner")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        BarcodeSannerView()
+    }
+}
